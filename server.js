ec299418f32996dc7d70171df2f21170f29ca86f
Mifras Gaffoor <81522853+mifrasgaffoor@users.noreply.github.com>
2021-09-23 12:23:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const ConnetDB = require("./config/db");
 const user = require("./routes/user");
 const mentor = require("./routes/mentor");
 const app = express();
-
 app.use(express.json());
 //DB Connection
 ConnetDB();
-
 //Routes api
 app.use("/user", user);
 app.use("/mentor", mentor);
-
 //Methods
 app.get("/", (req, res) => {
   res.send("hey this is home api");
